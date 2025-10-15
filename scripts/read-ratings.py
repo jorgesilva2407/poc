f0e13bb6486d8ce11d41780be8ef72c6aaa02ca7
jorgesilva2407 <jorge.lima2407@gmail.com>
2025-10-15 19:06:59 -0300

    Preprocess with LabelEncoder and save timestamp as well

--- scripts/read-ratings.py
+++ scripts/read-ratings.py
@@ -1,10 +1,11 @@
-import json
+import pickle as pkl
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 def read_ratings(ratings_file):
     df = pd.read_json(ratings_file, lines=True)
-    df = df[['reviewerID', 'asin', 'overall']]
-    df.columns = ['user_id', 'item_id', 'rating']
+    df = df[['reviewerID', 'asin', 'overall', 'unixReviewTime']]
+    df.columns = ['user_id', 'item_id', 'rating', 'timestamp']
     return df
 
 def merge_ratings(beauty_ratings, clothing_ratings):
@@ -12,27 +13,26 @@ def merge_ratings(beauty_ratings, clothing_ratings):
     return merged_ratings
 
 def index_column(df, column_name):
-    unique_values = df[column_name].unique()
-    value_to_index = {value: idx for idx, value in enumerate(unique_values)}
-    df[column_name] = df[column_name].map(value_to_index)
-    return df, value_to_index
+    encoder = LabelEncoder()
+    df[column_name] = encoder.fit_transform(df[column_name])
+    return df, encoder
 
 def save_ratings(ratings, output_file):
     ratings.to_csv(output_file, index=False)
 
-def save_index_mapping(mapping, output_file):
-    with open(output_file, 'w') as f:
-        json.dump(mapping, f)
+def save_encoder(encoder, output_file):
+    with open(output_file, 'wb') as f:
+        pkl.dump(encoder, f)
 
 def main() -> None:
     beauty_ratings = read_ratings('./data/raw/reviews_Beauty_5.jsonl')
     clothing_ratings = read_ratings('./data/raw/reviews_Clothing_Shoes_and_Jewelry_5.jsonl')
     merged_ratings = merge_ratings(beauty_ratings, clothing_ratings)
-    merged_ratings, user_id_index = index_column(merged_ratings, 'user_id')
-    merged_ratings, item_id_index = index_column(merged_ratings, 'item_id')
+    merged_ratings, user_encoder = index_column(merged_ratings, 'user_id')
+    merged_ratings, item_encoder = index_column(merged_ratings, 'item_id')
     save_ratings(merged_ratings, './data/processed/merged_ratings.csv')
-    save_index_mapping(user_id_index, './data/processed/user_id_index.json')
-    save_index_mapping(item_id_index, './data/processed/item_id_index.json')
+    save_encoder(user_encoder, './data/processed/user_encoder.pkl')
+    save_encoder(item_encoder, './data/processed/item_encoder.pkl')
 
 if __name__ == '__main__':
     main()
